05f144320daf7237ed862e42146fe206ba4c83b8
Mickaël Salaün <mic@digikod.net>
2023-08-08 21:16:46 +0200

    test-rust: Add initial script for rust-landlock
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ To make tests quick, interesting kernels are stored in kernels/artifacts and
 built with the kernels/make-uml.sh script.
 
 uml-run.sh can be used to launch an UML kernel with an init test script.
+
+## rust-landlock
+
+test-rust.sh can be used to test the Landlock crate against a specific kernel
+version:
+```shell
+cd rust-landlock
+.../test-rust.sh linux-6.1 2
+```
---
+++ test-rust.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright © 2023 Microsoft Corporation
+#
+# Example: CARGO="rustup run stable cargo" ./test-rust.sh linux-6.1 2
+
+set -e -u -o pipefail
+
+if [[ $# -ne 2 ]]; then
+	echo "usage: ${BASH_SOURCE[0]} <linux-uml-kernel> <landlock-abi>" >&2
+	exit 1
+fi
+
+KERNEL="$1"
+ABI="$2"
+
+# Enables to use rustup.
+if [[ -z "${CARGO:-}" ]]; then
+	CARGO="cargo"
+fi
+
+BASE_DIR="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
+
+${CARGO} test --no-run
+
+# Quickly run cargo test to get the built binary.
+EXEC="$(${CARGO} test --no-run --color never 2>&1 | sed -n 's,^ *Executable unittests src/lib.rs (\(.*\)) *$,\1,p')"
+
+if [[ -z "${EXEC}" ]]; then
+	echo "ERROR: Failed to get the test binary" >&2
+	exit 1
+fi
+
+OUT_RET="$(mktemp uml-ret.XXXXXXXXXX)"
+
+cleanup() {
+	rm -- "${OUT_RET}"
+}
+
+trap cleanup QUIT INT TERM EXIT
+
+"${BASE_DIR}/uml-run.sh" \
+	"${KERNEL}" \
+	"${EXEC}" \
+	"UML_RET=${OUT_RET}" \
+	"LANDLOCK_CRATE_TEST_ABI=${ABI}"
+
+exit "$(< "${OUT_RET}")"
