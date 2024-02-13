landlock-lsm/landlock-test-tools

commit a4c14fb6a04ab040a077d620b89d6a8f4300368b

uml,guest: Use a more explicit systemd configuration
Use a default target to not rely on the systemd.unit cmdline argument.
This is required to work in containers.

Make sure that /tmp is a tmpfs mount point (checked by Landlock's
base_test).

Signed-off-by: Mickaël Salaün <mic@digikod.net>

uml-run.sh
@@ -74,8 +74,7 @@ echo "[*] Booting kernel ${KERNEL}"
 	"rw" \
 	"console=tty0" \
 	"quiet" \
-	"systemd.unit=landlock-test.service" \
-	"SYSTEMD_UNIT_PATH=${BASE_DIR}/guest" \
+	"SYSTEMD_UNIT_PATH=${BASE_DIR}/guest/systemd" \
 	"PATH=${BASE_DIR}/guest:${PATH:-/usr/bin}" \
 	"TERM=${TERM:-linux}" \
 	"UML_UID=$(id -u)" \

guest/systemd/default.target
@@ -1,0 +1,2 @@
+[Unit]
+Description=Test environment

guest/systemd/default.target.wants/landlock-test.service
@@ -1,0 +1,1 @@
+../landlock-test.service

guest/uml-init.sh
@@ -51,6 +51,10 @@ if [[ "${HOME:-/}" == / ]]; then
 	export HOME="$(getent passwd "${UML_UID}" | cut -d: -f6)"
 fi
 
+if [[ -h /tmp ]]; then
+	echo "ERROR: /tmp must not be a symlink" >&2
+	exit_poweroff 1
+fi
 mount -t tmpfs -o "mode=1777,nosuid,nodev" tmpfs /tmp
 
 if [[ -z "${TMPDIR:-}" ]]; then

commit f9f28b209f74f002f85a1760fd67cda565eb5df4

uml: Fix /tmp check
Switch from glob to regex.

Fixes: a4c14fb6a04a ("uml,guest: Use a more explicit systemd configuration")
Signed-off-by: Mickaël Salaün <mic@digikod.net>

uml-run.sh
@@ -52,7 +52,7 @@ if [[ ! -f "${KERNEL}" ]]; then
 fi
 
 KERNEL_DIR="$(dirname -- "${KERNEL}")/"
-if [[ "${KERNEL_DIR}" =~ /tmp/*|/run/* ]]; then
+if [[ "${KERNEL_DIR}" =~ ^/(tmp|run)/ ]]; then
 	echo "ERROR: The kernel must not be in /tmp nor /run: ${KERNEL_DIR}" >&2
 	exit 1
 fi