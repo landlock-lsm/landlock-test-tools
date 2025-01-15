c2cc8787763a98a5ac676983c0a007750936421e
Mickaël Salaün <mic@digikod.net>
2025-01-15 17:50:10 +0100

    check-linux: Bump clang
    Bump clang-format last version to 19.
    
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- check-linux.sh
+++ check-linux.sh
@@ -216,7 +216,7 @@ check_format() {
 			trap unpatch_all QUIT INT TERM EXIT
 			git cat-file -p "${clang_format_compat}:.clang-format" > .clang-format
 		fi
-		local last_version="18"
+		local last_version="19"
 		local first_version="14"
 		local clang_format=""
 		local version
