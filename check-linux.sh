landlock-lsm/landlock-test-tools

commit bf17d203316769223371edf88234883dff1ff6ea

check-linux: Add a "stack" command to show stack usage delta
First, run:

  ./check-linux.sh build stack

...and then:

  ./check-linux.sh stack

Signed-off-by: Mickaël Salaün <mic@digikod.net>

check-linux.sh
@@ -35,6 +35,8 @@ export KBUILD_BUILD_USER="root"
 export KBUILD_BUILD_HOST="localhost"
 export KBUILD_BUILD_TIMESTAMP="$(git log --no-walk --pretty=format:%aD)"
 
+CURRENT_COMMIT="$(git --no-pager log --no-walk --max-count=1 --pretty=format:%h HEAD)"
+
 NPROC="$(nproc)"
 
 make_cmd() {
@@ -207,6 +209,53 @@ check_smatch() {
 	make_cmd CHECK="smatch -p=kernel" C=1 "${MAKE_ARGS[@]}"
 }
 
+# Records the stack usage for the current build and compare it to the parent.
+# When a parent reference is explicitly set, check_stack_landlock fails if
+# stack usage cannot be computed.
+check_stack_landlock() {
+	local parent_ref parent_set
+	local path="security/landlock/"
+	local current_usage="${O}/stackusage-${CURRENT_COMMIT}.txt"
+
+	if [[ -n "${1:-}" ]]; then
+		parent_ref="$1"
+		parent_set=true
+	else
+		parent_ref="HEAD~"
+		parent_set=false
+	fi
+
+	if [[ "${CC:-}" != "gcc" ]]; then
+		echo "[-] Stack usage needs CC=gcc" >&2
+		if ${parent_set}; then
+			exit 1
+		else
+			return
+		fi
+	fi
+
+	echo "[+] Checking stack usage: ${path}"
+	rm "${O}/${path}/"*.o 2>/dev/null || :
+	# See make_cmd:
+	./scripts/stackusage -o "${current_usage}" "-j${NPROC}" "ARCH=${ARCH}" "CC=${CC}" "O=${O}" "${path}"
+
+	local parent_commit parent_usage
+	# Pick the first parent.
+	parent_commit="$(git --no-pager log --no-walk --max-count=1 --pretty=format:%h "${parent_ref}")"
+	parent_usage="${O}/stackusage-${parent_commit}.txt"
+	if [[ -f "${parent_usage}" ]]; then
+		echo "[*] Stack delta between ${parent_commit} and ${CURRENT_COMMIT}:"
+		./scripts/stackdelta "${parent_usage}" "${current_usage}" | \
+			cut -f2- | \
+			sort -k4,4g | \
+			column -t |
+			sed 's/^/  /'
+	elif ${parent_set}; then
+		echo "[-] Failed to find parent stack usage: ${parent_usage}" >&2
+		exit 1
+	fi
+}
+
 check_format() {
 	if [[ -n "$(git --no-pager log --max-count=1 --grep '^landlock: Format with clang-format$' --pretty=format:%H v5.10..HEAD security/landlock)" ]]; then
 		echo "[+] Checking with clang-format: ${SOURCE_DIR}"
@@ -366,7 +415,7 @@ check_patch() {
 }
 
 exit_usage() {
-	echo "usage: $(basename -- "${BASH_SOURCE[0]}") all|build|build_light|lint|build_kselftest|kselftest|kunit|doc|patch..." >&2
+	echo "usage: $(basename -- "${BASH_SOURCE[0]}") all|build|build_light|lint|stack|build_kselftest|kselftest|kunit|doc|patch..." >&2
 	exit 1
 }
 
@@ -397,11 +446,20 @@ run() {
 				strip "${O}/linux"
 			fi
 			;;
+		stack)
+			# Needs a valid kernel configuration.
+			#
+			# When a parent reference is explicitly set,
+			# check_stack_landlock fails if stack usage cannot be
+			# computed.
+			check_stack_landlock ${PARENT_REF:-}
+			;;
 		lint)
 			install_headers
 			# tools/testing/selftests must go first because of patch_kselftest()
 			check_source_dir tools/testing/selftests/landlock
 			check_source_dir security/landlock
+			check_stack_landlock
 			check_source_dir samples/landlock
 			;;
 		build_kselftest)

commit 391fa3fe4524670c4e28526d24dcbb6afc21867b

check-linux: Fix potential spurious errors
Without argument, `return` returns the value of the last result.  For
instance, this always returns 1 for the "stack" command when CC is not
gcc.

Explicitly set the return value for all `return` references.

Fixes: bf17d2033167 ("check-linux: Add a "stack" command to show stack usage delta")
Signed-off-by: Mickaël Salaün <mic@digikod.net>

check-linux.sh
@@ -77,7 +77,7 @@ unpatch_all() {
 
 patch_kernel_kconfig() {
 	if [[ "${ARCH}" != "um" ]]; then
-		return
+		return 0
 	fi
 
 	if git apply "${BASE_DIR}/kernels/0001-test-Landlock-with-UML.patch" 2>/dev/null; then
@@ -89,7 +89,7 @@ patch_kernel_kconfig() {
 
 patch_samples_kconfig() {
 	if [[ "${ARCH}" != "um" ]]; then
-		return
+		return 0
 	fi
 
 	# Requires headers to be installed.
@@ -230,7 +230,7 @@ check_stack_landlock() {
 		if ${parent_set}; then
 			exit 1
 		else
-			return
+			return 0
 		fi
 	fi
 
@@ -294,7 +294,7 @@ check_build() {
 	if [[ "${ARCH}" == "um" ]]; then
 		# Only Kselftest builds without warning.
 		if [[ "${SOURCE_DIR##tools/}" == "${SOURCE_DIR}" ]]; then
-			return
+			return 0
 		else
 			patch_kselftest
 		fi
@@ -394,7 +394,7 @@ check_doc_path() {
 	local date="$(git log --no-walk '--date=format:%B %Y' --format=%ad HEAD -- "${path}")"
 
 	if [[ -z "${date}" ]]; then
-		return
+		return 0
 	fi
 
 	echo "[+] Checking date ${date} in ${path}"